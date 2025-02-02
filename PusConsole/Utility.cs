f6579238e48f7f30f4d26041b50d9077eed08230
Ahmad Syafiq <syafiq10191019@yahoo.com>
2025-02-02 20:17:03 +0700

    Add utility for generating random string

---
+++ PusConsole/Utility.cs
@@ -0,0 +1,16 @@
+public static class Utility
+{
+    public static string RandomString()
+    {
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        char[] stringChars = new char[8];
+        Random random = new();
+
+        for (int i = 0; i < stringChars.Length; i++)
+        {
+            stringChars[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new string(stringChars);
+    }
+}
